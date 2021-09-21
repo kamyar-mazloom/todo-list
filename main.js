25aa1bc1ced6d91aaf3e22825eb921c0452af331
Kamyar mazloom <mazloom1994@gmail.com>
2021-09-21 17:22:33 +0430

    Auto adding tasks to their related project / css tweak

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import {navbarCreation} from "/src/structure/navbarCreation.js";
 import{taskPageCreation} from "/src/structure/taskPageCreation.js";
 import{projectListCreation} from "/src/function/projectList.js";
+import { taskListCreation } from "../src/function/taskList.js";
 
 window.onload = (event) => {
     navbarCreation()
     taskPageCreation()
     projectListCreation()
+    taskListCreation()
 }
